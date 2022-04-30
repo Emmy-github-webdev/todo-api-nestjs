00b5dd21ebca67045189fab1afda0202f6f5723c
Emmy-github-webdev <ogahemmanuel75@gmail.com>
2022-04-30 15:32:10 +0000

    Implement JWT Passpoet strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     PassportModule,
-    UsersModule
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWTKEY,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRATION },
+    }),
   ],
   controllers: [
     AuthController
@@ -16,6 +22,7 @@ import { LocalStrategy } from './local.strategy';
   providers: [
     AuthService,
     LocalStrategy,
+    JwtStrategy
   ]
 })
 export class AuthModule {}
