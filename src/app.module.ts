dbee3ae6c7e01f1feee78d565d120ff407e90970
Emmy-github-webdev <ogahemmanuel75@gmail.com>
2022-04-30 14:17:34 +0000

    Add auth module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
 import { databaseConfig } from './database.config';
+import { User } from '../../users/user.entity';
 
 export const databaseProviders = [{
     provide: SEQUELIZE,
@@ -20,7 +21,7 @@ export const databaseProviders = [{
            config = databaseConfig.development;
         }
         const sequelize = new Sequelize(config);
-        sequelize.addModels(['models goes here']);
+        sequelize.addModels([User ]);
         await sequelize.sync();
         return sequelize;
     },
